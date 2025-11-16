dd1453dc569d91c46c02a4416ec77e40ae8da04d
mohammadkhaliljavdi <mohammadkhaliljavadi@gmail.com>
2025-11-16 09:41:05 +0330

    jk

--- script.js
+++ script.js
@@ -1,46 +1,101 @@
-let imgSrc = {
-    src: ["./img/0.jpg", "./img/1.jpg", "./img/2.jpg", "./img/3.jpg", "./img/4.jpg"],
-    Caption: ["Caption Text", "Caption Two", "Caption Three", "Caption four", "Caption five"]
+let imgSrc = [
+    { id: 1, src: "./img/0.jpg", Caption: "Caption Text" },
+    { id: 2, src: "./img/1.jpg", Caption: "Caption Two" },
+    { id: 3, src: "./img/2.jpg", Caption: "Caption Three" },
+    { id: 4, src: "./img/3.jpg", Caption: "Caption four" },
+    { id: 5, src: "./img/4.jpg", Caption: "Caption five" },
+    { id: 6, src: "./Screenshot 2025-11-16 061905.png", Caption: "Caption sixe" }
+]
+
+
+
+let arrowBox = document.querySelector(".arrowBox");
+let Slideshow = document.querySelector(".Slideshow");
+let counter = document.querySelector(".counter");
+let counting = document.querySelector(".counting");
+let length = document.querySelector(".length");
+let buttonSliderBox = document.querySelector(".buttonSliderBox")
+let x = 0;
+
+function update() {
+    counter.style.display = "block";
+    counting.innerHTML = imgSrc[x].id;
+    length.innerHTML = imgSrc.length;
+
+
 }
 
 
-let aroBox = document.querySelector(".aroBox")
-let Slideshow = document.querySelector(".Slideshow")
-let counting = document.querySelector(".counting")
-
-let x = 0;
-aroBox.addEventListener("click", function (e) {
-
+arrowBox.addEventListener("click", function (e) {
+    if (!e.target.classList.contains("arrowRight") & !e.target.classList.contains("leftArrow")) { return }
     if (e.target.classList.contains("arrowRight")) {
-        if (x < imgSrc.src.length - 1) {
+        if (x < imgSrc.length - 1) {
             x++
-            counting.innerHTML = x + 1
 
         } else {
             x = 0
-            counting.innerHTML = x + 1
         }
-
     }
 
 
     if (e.target.classList.contains("leftArrow")) {
         if (x > 0) {
             x--
-            counting.innerHTML = x + 1
 
         } else {
-            x = imgSrc.src.length - 1;
-            counting.innerHTML = x + 1
+            x = imgSrc.length - 1;
         }
-
     }
 
-
-   Slideshow.style.backgroundImage = `url(${imgSrc.src[x]})`
-   
+    update()
+    Slideshow.style.backgroundImage = `url(${imgSrc[x].src})`;
 
 })
+
+
+
+
+
+
+imgSrc.forEach((i) => {
+    buttonSliderBox.innerHTML += `<button atr="${i.id}"  class="" ></button>`
+})
+
+let buttonSlider = buttonSliderBox.querySelectorAll("button");
+buttonSlider.forEach((i) => {
+    i.addEventListener("click", () => {
+        let atr=i.getAttribute("atr");
+        atr=Number(atr);
+        let findId=imgSrc.find( (f) => f.id===atr);
+
+        if(findId){
+            console.log(atr);
+        }
+        
+    })
+})
+
+
+
+
+
+
+
+
+function wait() {
+    setInterval(() => {
+        if (x < imgSrc.length - 1) {
+            x++
+
+        } else {
+            x = 0
+        }
+        update()
+        Slideshow.style.backgroundImage = `url(${imgSrc[x].src})`;
+    }, 5000);
+}wait()
+
+
 
 
 
