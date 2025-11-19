782bde94ed8524ddcaa632879e91e6b53492af39
mohammadkhaliljavdi <mohammadkhaliljavadi@gmail.com>
2025-11-19 21:42:01 +0330

    PS C:\Users\khalil javadi\Desktop\w3j\How TO - Slideshow> git commit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let imgSrc = [
     { id: 3, src: "./img/2.jpg", Caption: "Caption Three" },
     { id: 4, src: "./img/3.jpg", Caption: "Caption four" },
     { id: 5, src: "./img/4.jpg", Caption: "Caption five" },
-    { id: 6, src: "./Screenshot 2025-11-16 061905.png", Caption: "Caption sixe" }
+    { id: 6, src: "./img/15.jpg", Caption: "Caption sixe" }
 ]
 
 
@@ -14,20 +14,47 @@ let Slideshow = document.querySelector(".Slideshow");
 let counter = document.querySelector(".counter");
 let counting = document.querySelector(".counting");
 let length = document.querySelector(".length");
-let buttonSliderBox = document.querySelector(".buttonSliderBox")
+let buttonSliderBox = document.querySelector(".buttonSliderBox");
+let CaptionText=document.querySelector(".CaptionText");
 let x = 0;
+let setval
+
+
+function startVal() {
+    setval = setInterval(() => {
+        if (x < imgSrc.length - 1) {
+            x++
+
+        } else {
+            x = 0
+        }
+        // Slideshow.style.backgroundImage = `url(${imgSrc[x].src})`;
+        update()
+    }, 5000);
+}
+startVal()
+
+
+function clearval() {
+    clearInterval(setval)
+    setTimeout(() => {
+        startVal()
+    }, 2000);
+}
+
+
 
 function update() {
     counter.style.display = "block";
     counting.innerHTML = imgSrc[x].id;
     length.innerHTML = imgSrc.length;
-
-
+    CaptionText.innerHTML=imgSrc[x].Caption;
+    Slideshow.style.backgroundImage = `url(${imgSrc[x].src})`;
 }
 
 
 arrowBox.addEventListener("click", function (e) {
-    if (!e.target.classList.contains("arrowRight") & !e.target.classList.contains("leftArrow")) { return }
+    if (!e.target.classList.contains("arrowRight") && !e.target.classList.contains("leftArrow")) { return }
     if (e.target.classList.contains("arrowRight")) {
         if (x < imgSrc.length - 1) {
             x++
@@ -48,8 +75,7 @@ arrowBox.addEventListener("click", function (e) {
     }
 
     update()
-    Slideshow.style.backgroundImage = `url(${imgSrc[x].src})`;
-
+    clearval()
 })
 
 
@@ -64,14 +90,20 @@ imgSrc.forEach((i) => {
 let buttonSlider = buttonSliderBox.querySelectorAll("button");
 buttonSlider.forEach((i) => {
     i.addEventListener("click", () => {
-        let atr=i.getAttribute("atr");
-        atr=Number(atr);
-        let findId=imgSrc.find( (f) => f.id===atr);
+        buttonSlider.forEach(btn => btn.classList.remove("checked"))
 
-        if(findId){
-            console.log(atr);
+        let atr = i.getAttribute("atr");
+        atr = Number(atr);
+        let findId = imgSrc.find((f) => f.id === atr);
+        x = atr-1;
+        if (findId) {
+            // console.log(imgSrc[x]);
+            update()
+            clearval()
+            i.classList.add("checked")
+            
         }
-        
+
     })
 })
 
@@ -82,18 +114,7 @@ buttonSlider.forEach((i) => {
 
 
 
-function wait() {
-    setInterval(() => {
-        if (x < imgSrc.length - 1) {
-            x++
 
-        } else {
-            x = 0
-        }
-        update()
-        Slideshow.style.backgroundImage = `url(${imgSrc[x].src})`;
-    }, 5000);
-}wait()
 
 
 
